4eaa71dc4a5581e09e9dc59b48f1cf729b594391
guillaume-LE-GOFF <guillzot@live.fr>
2019-02-22 21:13:37 +0100

    uplaod de l'ensemble du projet

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nuntium_site
 Site vitrine de présentation du cms perso : nuntium_cms 
+
+## Installation 
+Ouvrir le fichier installation.docx pour installer le cms
+( un peu d'indulgence ce projet n'est pas totalement fini il est en cours de développement)
---
+++ index.php
@@ -0,0 +1,259 @@
+﻿
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+    <title>
+      
+       Nuntium une nouvelle façon d'utiliser un site internet, créer le votre maintenant ! 
+      
+    </title>
+
+    
+      <link href='https://fonts.googleapis.com/css?family=Lora:400,400italic|Work+Sans:300,400,500,600' rel='stylesheet' type='text/css'>
+      <link href="assets/css/toolkit-startup.css" rel="stylesheet">
+      <link href="assets/css/application-startup.css" rel="stylesheet">
+    
+
+    
+
+    <style>
+      /* note: this is a hack for ios iframe for bootstrap themes shopify page */
+      /* this chunk of css is not part of the toolkit :) */
+      /* …curses ios, etc… */
+      @media (max-width: 768px) and (-webkit-min-device-pixel-ratio: 2) {
+        body {
+          width: 1px;
+          min-width: 100%;
+          *width: 100%;
+        }
+        #stage {
+          height: 1px;
+          overflow: auto;
+          min-height: 100vh;
+          -webkit-overflow-scrolling: touch;
+        }
+      }
+    </style>
+  </head>
+
+
+<body>
+  
+
+
+
+<div class="bsa bsb hidden" id="sidebar">
+  <ul class="nav bpi nav-stacked yl">
+    <li class="azb">Examples</li>
+    <li class="m">
+      <a class="qm active" href="index.html">Startup</a>
+    </li>
+    <li class="m">
+      <a class="qm" href="minimal/index.html">Minimal</a>
+    </li>
+    <li class="m">
+      <a class="qm" href="bold/index.html">Bold</a>
+    </li>
+    <li class="nav-divider"></li>
+    <li class="azb">Docs</li>
+    <li class="m">
+      <a class="qm" href="docs/index.html">Toolkit</a>
+    </li>
+    <li class="m">
+      <a class="qm" href="http://getbootstrap.com">Bootstrap</a>
+    </li>
+  </ul>
+</div>
+
+<div class="brz" id="stage">
+
+<div class="k am bqa bse"
+     style="background-image: url(assets/img/head.jpg);">
+
+  <div class="e aij aey app-navbar">
+  <nav class="du bqq azg qz">
+    <a class="l aiy" href="">
+      <strong style="background: #fff; padding: 12px; border-radius: 4px; color: #28669F;">Nuntium</strong>
+    </a>
+    <button
+      class="qv"
+      type="button"
+      data-target="#stage"
+      data-toggle="stage"
+      data-distance="-250">
+      <span class="qw"></span>
+    </button>
+
+    <div class="d-none wx axv">
+      <ul class="navbar-nav">
+        <li class="m ahq ">
+          <a class="qm" href="index.php">Accueil</a>
+        </li>
+        <li class="m ahq ">
+          <a class="qm" href="#block2">Design</a>
+        </li>
+        <li class="m ahq ">
+          <a class="qm" href="#block3">
+          Technologie
+</a>
+        </li>
+        <li class="m ahq ">
+          <a class="qm" href="#block4">
+Recommandation
+</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</div>
+
+  <div class="bqg ait">
+    <div class="e">
+      <div class="c">
+        <div class="fv gt">
+          <h1 class="bsf bsg">Découvrez Nuntium</h1>
+          <p class="al agw an">Nuntium est un logiciel libre utilisé pour créer de superbes sites, blogs ou applications.</p>
+          <a href="install.rar" class="dm ap" style="padding: 12px 50px;color: #fff;background-color: #2595FF;border-color: #2595FF;">Essayer maintenant !</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="k block-secondary aa" id="block2">
+  <div class="e">
+    <div class="c aq">
+
+      <div class="fq d-none wx">
+        <img class="bsh" src="assets/img/startup-2.jpg" style="width: 100%;">
+      </div>
+
+      <div class="gf fx ajb">
+        <h6 class="an axv">Responsive design</h6>
+        <h2>Un site internet qui s'adapte sur tous type d'écran</h2>
+        <p class="al agw">Nous combinons des événements en temps réel immédiats avec des données historiques riches pour répondre aux questions les plus difficiles concernant la rétention, la croissance et l'engagement</p>
+        <div class="c d-none xb">
+          <div class="fr agp">
+            <h5>Simplicité</h5>
+            <p>Nous recherchons des données sur une milliseconde. Vous pouvez réagir aux nouvelles informations en secondes plutôt qu'en jours.</p>
+          </div>
+          <div class="fr">
+            <h5>Autonomie</h5>
+            <p>Nous traitons nos données sur un réseau massivement distribué de serveurs fiables pour garantir une disponibilité de 99,99%.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="k am block-secondary ab" id="block3">
+  <div class="e">
+    <div class="c aq">
+      <div class="gf hu jv">
+        <h6 class="an axv">Qualité</h6>
+        <h2>Ne vous souciez du code informatique</h2>
+        <p class="al agw an">Pas de besoin d'être un "geek" pour avoir son propre site internet.
+        Avec Nuntium vous gerer facilement et intuitivement votre site internet, creer autant de contenu que vous voulez !</p>
+        <a href="installation.pdf" class="dm ap" style="padding: 12px 50px;color: #fff;background-color: #2595FF;border-color: #2595FF;">Télécharger la doc</a>
+      </div>
+      <div class="ge ajb ht jw">
+        <pre class="ac">
+<span>1</span> <span>Nuntium</span> “Qui sont les 3 derniers utilisateurs”
+<span>2</span>
+<span>3</span>  {
+<span>4</span>    "Dave": {
+<span>5</span>      "prénom": "Dave Gamache",
+<span>6</span>      "adresse": "@dhg",
+<span>7</span>    }
+<span>8</span>    "Mark": {
+<span>9</span>      "prénom": "Mark Otto",
+<span>10</span>      "adresse": "@mdo",
+<span>11</span>    }
+<span>12</span>    "Jacob": {
+<span>13</span>      "prénom": "Jacob Thornton",
+<span>14</span>      "adresse": "@fat",
+<span>15</span>    }
+<span>16</span>  }</pre>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="k am ad"
+     style="background-image: url(assets/img/startup-3.jpg);"
+     id="block4">
+    <div class="e">
+      <div class="c aq aic yt">
+        <div class="fs ge aiq">
+          <h6 class="an axv agi">Recommandation</h6>
+          <h2 class="agw">“Nuntium est incroyable. Faire son site internet soit même ne prend maintenant que quelques minutes et est disponibles pour tous !”</h2>
+          <p class="agw an">Cindy Smith, Utilisateur du réseau Nuntium</p>
+        </div>
+      </div>
+  </div>
+</div>
+
+
+<div class="k am ak">
+ <div class="e">
+    <div class="c">
+      <div class="ge ahd">
+        <ul class="ek ayn">
+          <li class="agi"><h6 class="axv">A propos</h6></li>
+          <li class="an">
+          Nous travaillons sur Nuntium depuis une quelque années et sommes très fiers de ce que nous avons créé. Si vous souhaitez en savoir plus ou être intéressé par cet outil, contactez-nous à tout moment au <a href="mailto: Nuntium@society.fr">Nuntium@society.fr</a>.
+          </li>
+        </ul>
+      </div>
+      <div class="gb ajb ahd">
+        <ul class="ek ayn">
+          <li class="agi"><h6 class="axv">Produit</h6></li>
+          <li class="an">Responsive</li>
+          <li class="an">Design</li>
+          <li class="an">Simplicité</li>
+          <li class="an">Galerie</li>
+        </ul>
+      </div>
+      <div class="gb ahd">
+        <ul class="ek ayn">
+          <li class="agi"><h6 class="axv">Utilisateur</h6></li>
+          <li class="an">Professionel</li>
+          <li class="an">Administration</li>
+          <li class="an">Ecole</li>
+          <li class="an">Particulier</li>
+        </ul>
+      </div>
+      <div class="gb ahd">
+        <ul class="ek ayn">
+          <li class="agi"><h6 class="axv">Mentions</h6></li>
+          <li class="an">Termes</li>
+          <li class="an">Legales</li>
+          <li class="an">Privée</li>
+          <li class="an">License</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+    <script>
+      // execute/clear BS loaders for docs
+      $(function(){while(window.BS&&window.BS.loader&&window.BS.loader.length){(window.BS.loader.pop())()}})
+    </script>
+</body>
+</html>
+
